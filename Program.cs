93c44bf79cd652fbc47c9d518d4b5ee6ae5402f5
Tw212 <Tewe07001@utb.vaxjo.se>
2024-10-09 11:18:36 +0200

    dfad

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("skriv din först din längd och sedan din vikt");
+string text= Console.ReadLine();
+string text2 = Console.ReadLine();
+double tal = double.Parse(text);
+double tal2 = double.Parse(text2);
+    if(tal2/(tal*tal>0,00185 tal2/(tal*tal)<0,00249){
+
+    }
